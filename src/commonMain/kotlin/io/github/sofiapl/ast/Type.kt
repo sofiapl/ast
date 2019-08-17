2e20ce2b848fef7fcdb8bfda17fd69fe491f23fd
Eridan Domoratskiy <eridan200@mail.ru>
2019-08-17 23:22:54 +0300

    Add fictive type specifications. Add EOLs at EOFs

--- src/commonMain/kotlin/io/github/sofiapl/ast/Type.kt
+++ src/commonMain/kotlin/io/github/sofiapl/ast/Type.kt
@@ -2,5 +2,6 @@ package io.github.sofiapl.ast
 
 data class Type(
     val name: List<String>,
+    val fictiveTypes: List<SpecificationFictiveType>?,
     val nullable: Boolean
 ): Node
--- src/commonMain/kotlin/io/github/sofiapl/ast/expr/Invoke.kt
+++ src/commonMain/kotlin/io/github/sofiapl/ast/expr/Invoke.kt
@@ -2,8 +2,10 @@ package io.github.sofiapl.ast.expr
 
 import io.github.sofiapl.ast.Expr
 import io.github.sofiapl.ast.InvocationParameter
+import io.github.sofiapl.ast.SpecificationFictiveType
 
 data class Invoke(
     val expr: Expr,
+    val fictiveTypes: List<SpecificationFictiveType>?,
     val parameters: List<InvocationParameter>
 ): Expr
