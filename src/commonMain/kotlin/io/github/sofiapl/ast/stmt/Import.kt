d1496b4b26112e1cb147201ea98b5f2fb536086e
Eridan Domoratskiy <eridan200@mail.ru>
2019-08-23 11:43:13 +0300

    Add import and package syntax

--- src/commonMain/kotlin/io/github/sofiapl/ast/stmt/Import.kt
+++ src/commonMain/kotlin/io/github/sofiapl/ast/stmt/Import.kt
@@ -2,4 +2,5 @@ package io.github.sofiapl.ast.stmt
 
 import io.github.sofiapl.ast.Stmt
 
+// "import" <name through ".">
 data class Import(val name: List<String>): Stmt
--- src/commonMain/kotlin/io/github/sofiapl/ast/stmt/Package.kt
+++ src/commonMain/kotlin/io/github/sofiapl/ast/stmt/Package.kt
@@ -2,4 +2,5 @@ package io.github.sofiapl.ast.stmt
 
 import io.github.sofiapl.ast.Stmt
 
+// "package" <name through ".">
 data class Package(val name: List<String>): Stmt
